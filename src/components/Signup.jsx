ba44b15b94b055a61631554f1f808f090af7b33f
OwenLobato <owenlobatov@gmail.com>
2023-11-04 04:39:56 -0600

    Add signup form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,71 @@
+import { useState } from 'react';
+import { Box, TextField, Button, Typography } from '@mui/material';
+
 export const Signup = () => {
-  return <div>Signup</div>;
+  const initialInputs = {
+    name: '',
+    email: '',
+    password: '',
+  };
+  const [inputs, setInputs] = useState(initialInputs);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('INPUTS: ', inputs);
+  };
+
+  const handleChangeInput = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: 300,
+            mx: 'auto',
+          }}
+        >
+          <Typography variant='h2'>Sign up</Typography>
+          <TextField
+            name='name'
+            value={inputs.name}
+            onChange={handleChangeInput}
+            margin='normal'
+            variant='outlined'
+            placeholder='Name'
+          />
+          <TextField
+            name='email'
+            value={inputs.email}
+            onChange={handleChangeInput}
+            margin='normal'
+            variant='outlined'
+            placeholder='Email'
+            type='email'
+          />
+          <TextField
+            name='password'
+            value={inputs.password}
+            onChange={handleChangeInput}
+            margin='normal'
+            variant='outlined'
+            placeholder='Password'
+            type='password'
+          />
+          <Button variant='contained' type='submit'>
+            Sign up
+          </Button>
+        </Box>
+      </form>
+    </div>
+  );
 };
