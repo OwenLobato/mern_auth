0de31bfa7c21cd9c3e90dd3737734d693ab82d2e
OwenLobato <owenlobatov@gmail.com>
2023-11-04 02:39:33 -0600

    Add login authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,3 +30,24 @@ export const signup = async (req, res, next) => {
 
   return res.status(201).json({ message: user });
 };
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await userModel.findOne({ email: email });
+  } catch (err) {
+    return new Error(err);
+  }
+
+  if (!existingUser) {
+    return res.status(400).json({ message: 'User not found. Signup' });
+  }
+
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: 'Invalid email/password' });
+  }
+  return res.status(200).json({ message: 'Successfully logged in' });
+};
