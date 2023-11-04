eb431f9f01f85b4f1e9b546fcef0902be0703d0a
OwenLobato <owenlobatov@gmail.com>
2023-11-04 12:17:59 -0600

    Add get user by cookie

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const mongoUri = process.env.MONGODB_URI;
 const port = process.env.PORT || 9999;
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api', router);
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,3 +1,34 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
+
 export const Welcome = () => {
-  return <div>Welcome</div>;
+  const [user, setUser] = useState();
+
+  const sendRequest = async () => {
+    const apiURL = process.env.REACT_APP_API_URL;
+    const port = process.env.REACT_APP_PORT;
+    const apiVersion = process.env.REACT_APP_API_VERSION;
+
+    const res = await axios
+      .get(`${apiURL}:${port}/${apiVersion}/user`, {
+        withCredentials: true,
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+
+    const data = await res.data;
+    return data;
+  };
+
+  useEffect(() => {
+    sendRequest()
+      .then((data) => {
+        setUser(data.user);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <div>{user && <h1>{user.name}</h1>}</div>;
 };
