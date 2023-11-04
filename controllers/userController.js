6e0052a8e746a362915c5b936f79ebf6a47e96df
OwenLobato <owenlobatov@gmail.com>
2023-11-04 02:10:18 -0600

    Register user if not exists

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,20 @@ import { userModel } from '../models/User.js';
 
 export const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await userModel.findOne({ email: email });
+  } catch (err) {
+    console.log('Error searching user', err);
+  }
+
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ message: 'User already exists, login instead' });
+  }
+
   const user = new userModel({ name, email, password });
 
   try {
