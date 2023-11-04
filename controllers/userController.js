93504562f8885ee095d4136d3a5b48857066a805
OwenLobato <owenlobatov@gmail.com>
2023-11-04 02:30:43 -0600

    Add hashed password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import { userModel } from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 export const signup = async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -16,7 +17,10 @@ export const signup = async (req, res, next) => {
       .json({ message: 'User already exists, login instead' });
   }
 
-  const user = new userModel({ name, email, password });
+  const salt = bcrypt.genSaltSync(Number(process.env.SALT_ROUNDS));
+  const hashedPassword = bcrypt.hashSync(password, salt);
+
+  const user = new userModel({ name, email, password: hashedPassword });
 
   try {
     await user.save();
