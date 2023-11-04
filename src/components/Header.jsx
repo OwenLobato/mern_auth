32db1e363e745bdceb9995b7568c243e161fb31c
OwenLobato <owenlobatov@gmail.com>
2023-11-04 04:07:15 -0600

    Add MUI header

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import { Box, AppBar, Toolbar, Typography, Tab, Tabs } from '@mui/material';
+
+export const Header = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Typography variant='h3'>MERN Auth</Typography>
+          <Box sx={{ ml: 'auto' }}>
+            <Tabs
+              indicatorColor='secondary'
+              textColor='inherit'
+              onChange={(e, val) => setValue(val)}
+              value={value}
+            >
+              <Tab label='Login' />
+              <Tab label='Signup' />
+            </Tabs>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
